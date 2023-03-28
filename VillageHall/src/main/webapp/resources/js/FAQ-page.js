d5b240d4499c4799a7fbe98cb7642b776dc793ca
Apsalus <allnight724@gmail.com>
2023-03-28 12:46:43 +0900

    css추가

--- VillageHall/src/main/webapp/resources/js/FAQ-page.js
+++ VillageHall/src/main/webapp/resources/js/FAQ-page.js
@@ -24,3 +24,15 @@ tabContents.forEach(function(content) {
     content.style.display = "none";
   }
 });
+
+$(function () {
+  $(".FAQ-Ul1").on("click", function() {
+    console.log($(this).next())
+    if($(this).next().css("display") == 'none') {
+      $(this).siblings("div").slideUp();
+      $(this).next().slideDown();
+    } else{
+      $(this).next().slideUp();
+    }
+  })
+})
