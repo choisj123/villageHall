aa97fc3cb16f40907c0ccfcda10b27ee676f3841
DESKTOP-HHMVOCR\JeongYoon <kjyoon811@gmail.com>
2023-03-31 21:00:35 +0900

    회원가입 완료시 완료/실패창 띄우기 추가완료

--- VillageHall/src/main/java/com/kh/villagehall/user/controller/SendEmailServlet.java
+++ VillageHall/src/main/java/com/kh/villagehall/user/controller/SendEmailServlet.java
@@ -63,7 +63,7 @@ public class SendEmailServlet extends HttpServlet{
       // 1. 구글 계정 생성(기존 이메일 사용해도됨)
       // 2. 계정 -> 보안 설정 진행  
       //  1) 2단계 인증 추가
-      //  2) 앱 비밀번호 생성(메일, 서버컴퓨터 OS) -> 저장해두기 ( queapyfpwuqdpisq )
+      //  2) 앱 비밀번호 생성(메일, 서버컴퓨터 OS) -> 저장해두기 
       
       
       final String smtpEmail = prop.getProperty("smtpEmail"); // 이메일
