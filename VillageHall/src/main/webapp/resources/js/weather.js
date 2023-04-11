697ac4604195541cbf309843c03608c0d5f8f146
kms <kimrlaks@gmail.com>
2023-04-11 17:50:32 +0900

    test

--- VillageHall/src/main/webapp/resources/js/weather.js
+++ VillageHall/src/main/webapp/resources/js/weather.js
@@ -6,9 +6,11 @@
         var $main = data.weather[0].main;
         var $humidity = data.main.humidity + '%';
         var $now = new Date($.now());
+        var $speed = Math.floor(data.wind.speed) + 'm/s';
         var $cDate = $now.getMonth() + 1 + '월' + $now.getDate() + '일';
         //  + $now.getHours()+ '시간' + $now.getMinutes() + '분';
         var $wIcon = data.weather[0].icon;
+        var $name = data.name;
 
 
         var box = document.querySelector(".box");
@@ -40,6 +42,8 @@
         $('.main').append(getText($main));
         $('.ctemp').append($cTemp);
         $('.humidity').append($humidity);
+        $('.speed').append($speed);
+        $('.name').append($name);
         $('.cicon').append(' <img src="http://openweathermap.org/img/wn/'+ $wIcon + '.png"/>');
          
         
